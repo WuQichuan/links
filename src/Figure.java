e0bbc56beade703f218e16490aaae733ede75fcb
WuQichuan <1062112233@qq.com>
2018-10-11 15:52:19 +0800

    修复已知BUG

--- src/Figure.java
+++ src/Figure.java
@@ -20,6 +20,10 @@ public class Figure {
     public boolean LinksToEliminate(Board board,Point a, Point b){
         System.out.println("第一个点的值为："+board.getArr()[a.x][a.y]);
         System.out.println("第二个点的值为："+board.getArr()[b.x][b.y]);
+        //如果两个点值不相等则不能连接消灭
+        if(!board.getArr()[a.x][a.y].equals(board.getArr()[b.x][b.y])  ){
+            return false;
+        }
         //判断直线是否能连通
         if(noTurn( board,a,b)){
             System.out.println("直线连通解决问题");
@@ -55,25 +59,25 @@ public class Figure {
     }
 
     boolean noTurn(Board board,Point a,Point b){
-        //如果两个点值不相等
-        if(!board.getArr()[a.x][a.y].equals(board.getArr()[b.x][b.y])  ){
-            //两点不等但是两点其一有等于0的说明可以连通，返回true
-            if(board.getArr()[a.x][a.y] == 0|| board.getArr()[b.x][b.y] == 0){
-                return true;
-            }
-            //否则返回false
+        System.out.println("直线第一个点的值为："+board.getArr()[a.x][a.y]+"坐标为："+a);
+        System.out.println("直线第二个点的值为："+board.getArr()[b.x][b.y]+"坐标为："+b);
+        //判断自己不能消灭自己
+        if(a.x == b.x && a.y == b.y){
             return false;
         }
+
         //如果横向相等，a.y = by
         if(a.y == b.y){
             //如果a在b的左面
             if(a.x < b.x){
                 //说明二者相邻
                 if(a.x +1 ==b.x){
+                    System.out.println("当前直线成功原因：二者相邻");
                     return true;
                 }
                 //遍历a到b之间的路径
                 for(int i = a.x+1;i<b.x;i++){
+                    System.out.println("当前路径的点的值为："+board.getArr()[i][a.y]);
                     //路径中如果有不为0的说明路径不通无法连接
                     if(board.getArr()[i][a.y] != 0){
                         System.out.println("当前路径失败，路径不通："+board.getArr()[i][a.y]);
@@ -83,27 +87,31 @@ public class Figure {
             }else{
                 //说明二者相邻
                 if(b.x +1 ==a.x){
+                    System.out.println("当前直线成功原因：二者相邻");
                     return true;
                 }
                 for(int i = b.x+1;i<a.x;i++){
+                    System.out.println("当前路径的点的值为："+board.getArr()[i][a.y]);
                     if(board.getArr()[i][a.y] != 0){
                         System.out.println("当前路径失败，路径不通："+board.getArr()[i][a.y]);
                         return false;
                     }
                 }
             }
+            System.out.println("当前直线成功原因：路径中不存在不为0的数据");
             return true;
         }
 
         //纵向相等a.x = b.x
         if(a.x == b.x){
-
             if(a.y < b.y){
                 //说明二者相邻
                 if(a.y +1 ==b.y){
+                    System.out.println("当前直线成功原因：二者相邻");
                     return true;
                 }
                 for(int i = a.y+1;i<b.y;i++){
+                    System.out.println("当前路径的点的值为："+board.getArr()[a.x][i]);
                     if(board.getArr()[a.x][i] != 0){
                         System.out.println("当前路径失败，路径不通："+board.getArr()[a.x][i]);
                         return false;
@@ -112,15 +120,18 @@ public class Figure {
             }else{
                 //说明二者相邻
                 if(b.y +1 ==a.y){
+                    System.out.println("当前直线成功原因：二者相邻");
                     return true;
                 }
                 for(int i = b.y+1;i<a.y;i++){
+                    System.out.println("当前路径的点的值为："+board.getArr()[a.x][i]);
                     if(board.getArr()[a.x][i] != 0){
                         System.out.println("当前路径失败，路径不通："+board.getArr()[a.x][i]);
                         return false;
                     }
                 }
             }
+            System.out.println("当前直线成功原因：路径中不存在不为0的数据");
             return true;
         }
         System.out.println("目前无法直线链接，当前两点不在同一直线");
@@ -135,15 +146,22 @@ public class Figure {
         System.out.println("当前一折点可尝试的两个折点：");
         System.out.println("C :"+c.toString());
         System.out.println("D :"+d.toString());
-        //如果C能和AB分别直线连通说明C作为折点可行，返回true
-        if(noTurn(board,a,c) && noTurn(board,c,b)){
-            return true;
+        //判断折点是否为可连通点
+        if(board.getArr()[c.x][c.y] == 0 ){
+            //如果C能和AB分别直线连通说明C作为折点可行，返回true
+            if(noTurn(board,a,c) && noTurn(board,c,b)){
+                return true;
+            }
+        }
+        //判断折点是否为可连通点
+        if(board.getArr()[d.x][d.y] == 0){
+            //如果D能和AB分别直线连通说明C作为折点可行，返回true
+            if(noTurn(board,a,d) && noTurn(board,d,b)){
+                return true;
+            }
         }
 
-        //如果D能和AB分别直线连通说明C作为折点可行，返回true
-        if(noTurn(board,a,d) && noTurn(board,d,b)){
-            return true;
-        }
+
 
         //C和D都不能作为折点的话说明AB直接无法一折连通
         return false;
@@ -156,26 +174,39 @@ public class Figure {
             //在垂直水平线中找出水平连通线
             Point c1 = new Point(a.x,i);
             Point c2 = new Point(b.x,i);
-            //说明该平行线可连通
-            if(noTurn(board,c1,c2)){
-                //再去判断C1和A，   C2和B是否可连通
-                if(noTurn(board,c1,a) && noTurn(board,c2,b)){
-                    return true;
+            //1，首先判断折点是否是空点（折点不能作为实际有值得点存在）
+            if(board.getArr()[c1.x][c1.y] == 0 && board.getArr()[c2.x][c2.y] == 0){
+                //2,再判断两折点连线是否可以连通
+                if(noTurn(board,c1,c2)){
+                    System.out.println(c1+"和"+c2+"两个折点可以连通");
+                    //3,最后判断C1和A，   C2和B是否可连通
+                    if(noTurn(board,c1,a) && noTurn(board,c2,b)){
+                        System.out.println("折点与其他点也可以连通,二折点成功");
+                        return true;
+                    }
                 }
             }
-
             //在水平线中超出垂直连通线
             Point d1 = new Point(i,a.y);
             Point d2 = new Point(i,b.y);
-            //说明该平行线可连通
-            if(noTurn(board,d1,d2)){
-                //再去判断D1和A，   D2和B是否可连通
-                if(noTurn(board,d1,a) && noTurn(board,d2,b)){
-                    return true;
+            //1，首先判断折点是否是空点（折点不能作为实际有值de点存在）
+            if(board.getArr()[d1.x][d1.y] == 0 && board.getArr()[d2.x][d2.y] == 0){
+                //2,再判断两折点连线是否可以连通
+                if(noTurn(board,d1,d2)){
+                    System.out.println(d1+"和"+d2+"两个折点可以连通");
+                    //3,最后判断D1和A，   D2和B是否可连通
+                    if(noTurn(board,d1,a) && noTurn(board,d2,b)){
+                        System.out.println("折点与其他点也可以连通，二折点成功");
+                        return true;
+                    }
                 }
             }
-
         }
+        return false;
+    }
+
+    boolean shortTwoTurn(Board board,Point a,Point b){
+
         return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         int edgNum = 4;
-        Board board = new Board(4);
+        int edgNumPlus = 2;
+        Board board = new Board(edgNum);
         Figure figure = new Figure();
         while (true){
             System.out.println("-----当前棋盘-----");
@@ -15,12 +16,24 @@ public class Main {
             Scanner scan = new Scanner(System.in);
             System.out.println("请输入第一个坐标的X :");
             a.x = scan.nextInt();
+            if(a.x >edgNum+edgNumPlus-1){
+                continue;
+            }
             System.out.println("请输入第一个坐标的Y :");
             a.y = scan.nextInt();
+            if(a.y >edgNum+edgNumPlus-1){
+                continue;
+            }
             System.out.println("请输入第二个坐标的X :");
             b.x = scan.nextInt();
+            if(b.x >edgNum+edgNumPlus-1){
+                continue;
+            }
             System.out.println("请输入第二个坐标的Y :");
             b.y = scan.nextInt();
+            if(b.y >edgNum+edgNumPlus-1){
+                continue;
+            }
             if(figure.LinksToEliminate(board,a,b)){
                 System.out.println("连接消除成功，请继续");
             }else{
